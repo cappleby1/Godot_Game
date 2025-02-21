1b725cc7a99304120722edb9c5865006b126b16c
Cat Appleby <catappleby1@proton.me>
2025-02-21 18:08:22 +0000

    Character movement & platforms

--- godot_project/scripts/PlayerMovement.gd
+++ godot_project/scripts/PlayerMovement.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-@export var speed = 400
-
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
+const GRAVITY = 300.0
+const WALK_SPEED = 300.0
+const JUMP_SPEED = 200.0
 
 func _physics_process(delta):
-	get_input()
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("move_left"):
+		velocity.x =- WALK_SPEED
+	elif Input.is_action_pressed("move_right"):
+		velocity.x =+ WALK_SPEED
+	elif Input.is_action_pressed("move_up") and is_on_floor():
+		velocity.y =- JUMP_SPEED
+	else:
+		velocity.x = 0
+
 	move_and_slide()
