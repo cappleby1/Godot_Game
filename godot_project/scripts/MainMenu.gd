13d37d922396c92684362f7fb1d28f4596ee47e5
Cat Appleby <catappleby1@proton.me>
2025-02-22 09:32:55 +0000

    Created options menu with back button that can be accessed from main menu

--- godot_project/scripts/MainMenu.gd
+++ godot_project/scripts/MainMenu.gd
@@ -4,7 +4,7 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Levels/Level1.tscn")
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/UI/OptionsMenu.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
